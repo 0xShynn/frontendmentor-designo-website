97f52040ecf5d091af2ac7df82ac79092b3ca83d
Antonin Nhek <a.nhek@icloud.com>
2021-09-28 16:52:29 +0200

    updated: design system page

--- src/pages/designsystem.js
+++ src/pages/designsystem.js
@@ -1,4 +1,16 @@
-import { Box, Heading, HStack, Link, Text, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 const DesignSystem = () => {
   return (
@@ -18,14 +30,47 @@ const DesignSystem = () => {
           approach in all of our projects and value customer collaboration. It
           guarantees superior results that fulfill our clients’ needs.
         </Text>
-        <HStack>
-          <Link href="#" variant="dark">
-            Learn More
-          </Link>
-          <Link href="#" variant="light">
-            Learn More
-          </Link>
+        <HStack spacing="4">
+          <Flex bg="black" p="4">
+            <Link href="#" variant="dark">
+              Learn More
+            </Link>
+          </Flex>
+          <Flex bg="white" p="4" borderWidth="1px">
+            <Link href="#" variant="light">
+              Learn More
+            </Link>
+          </Flex>
         </HStack>
+        <Box bg="primary.peach" w="full" p="6">
+          <form>
+            <FormControl id="name" isRequired>
+              <FormLabel color="white">Name</FormLabel>
+              <Input
+                type="name"
+                variant="flushed"
+                placeholder="Name"
+                _placeholder={{ color: 'white', opacity: '0.5' }}
+                color="white"
+                px="4"
+              />
+            </FormControl>
+            <FormControl>
+              <Input
+                type="email"
+                variant="flushed"
+                required
+                placeholder="Email"
+                _placeholder={{ color: 'white', opacity: '0.5' }}
+                color="white"
+                px="4"
+              />
+            </FormControl>
+            <Button type="submit" mt="6">
+              Send
+            </Button>
+          </form>
+        </Box>
       </VStack>
     </Box>
   )
