207c884d8f04723d6652cc0dd11e820edd141a7c
Antonin Nhek <a.nhek@icloud.com>
2021-10-04 20:02:34 +0200

    style: hero comp (all size)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { Box, Flex, Heading, Link, Text } from '@chakra-ui/layout'
 import { NextSeo } from 'next-seo'
-import NextImage from 'next/image'
 
 import Layout from '../components/Layout'
 import CustomImage from '../components/utils/CustomImage'
@@ -34,54 +33,66 @@ export default function Home({ data, page }) {
       >
         <Flex
           bg="primary.peach"
-          direction={{ base: 'column', xl: 'row' }}
-          px={{ base: 6, xl: 24 }}
-          py={{ base: 16, md: 16, xl: 36 }}
+          direction={{ base: 'column', lg: 'row' }}
+          px={{ base: 6, lg: 24 }}
+          py={{ base: 16, md: 16, lg: 28, xl: 36 }}
+          mb={{ base: 0, md: 24 }}
           rounded={{ base: 'none', md: '2xl' }}
           w="full"
-          h={{ base: '843px' }}
+          h={{ base: '843px', lg: 'auto' }}
           pos="relative"
           overflow="hidden"
-          mb="24"
           bgImage={'images/bg-pattern-hero-home.svg'}
-          bgPos={{ base: '0 90px', md: '205px 105px', xl: 'right' }}
+          bgPos={{
+            base: '0 90px',
+            md: '205px 105px',
+            lg: '405px 105px',
+            xl: '100% 0',
+          }}
           bgRepeat="no-repeat"
           align="center"
         >
           <Box
-            maxW="540px"
-            textAlign={{ base: 'center', xl: 'left' }}
+            maxW={{ base: '540px', lg: '440px', xl: '540px' }}
+            textAlign={{ base: 'center', lg: 'left' }}
             mx="auto"
           >
             <Heading as="h1" variant="h1" color="white" mb="8">
               {heroHome.title}
             </Heading>
+
             <Text
               color="white"
               maxW="445px"
-              mb="6"
+              mb={{ base: 6, lg: 8 }}
               mx={{ base: 'auto', xl: 0 }}
             >
               {heroHome.description}
             </Text>
+
             <Link
               textTransform="uppercase"
               variant="dark"
               display="inline-block"
-              mb="12"
+              mb={{ base: 12, lg: 0 }}
             >
               {heroHome.button.label}
             </Link>
           </Box>
-          <Flex w="200%" justify="center" pos="relative" top="-150px">
+
+          <Flex
+            w={{ base: '200%', lg: '100%' }}
+            justify="center"
+            pos={{ base: 'relative', lg: 'unset' }}
+            top="-150px"
+          >
             <CustomImage
-              pos={{ base: 'static', xl: 'absolute' }}
-              top={{ md: 0, xl: '-50px' }}
-              right={{ md: 0, xl: '-70px' }}
+              pos={{ base: 'static', lg: 'absolute' }}
+              top={{ base: 0, lg: '-50px', xl: '-30px' }}
+              right={{ base: 0, lg: '-70px', xl: '-70px' }}
               image={heroHome.image.url}
               width={heroHome.image.width}
               height={heroHome.image.height}
-              display={{ md: 'inline-block' }}
             />
           </Flex>
         </Flex>
