b81b5778c3d8c661b185c746f0b4497a5e498cfd
Antonin Nhek <a.nhek@icloud.com>
2021-10-06 16:04:36 +0200

    fix: banner margin

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,20 +2,20 @@ import { Box } from '@chakra-ui/react'
 
 const Banner = ({ children }) => {
   return (
-    <Box
-      color="white"
-      px={{ base: 6, lg: 20 }}
-      py="16"
-      bg="primary.peach"
-      bgImage="/images/bg-pattern-call-to-action.svg"
-      bgPos={{ base: '45% center', md: '0% center', lg: 'center right' }}
-      bgRepeat="no-repeat"
-      rounded="2xl"
-      mx={{ base: 6, xl: 'auto' }}
-      pos="relative"
-      maxW="1110px"
-    >
-      {children}
+    <Box maxW="1110px" mx={{ base: 6, lg: 'auto' }} px={{ base: 0, lg: 8 }}>
+      <Box
+        color="white"
+        px={{ base: 6, lg: 20 }}
+        py="16"
+        bg="primary.peach"
+        bgImage="/images/bg-pattern-call-to-action.svg"
+        bgPos={{ base: '45% center', md: '0% center', lg: 'center right' }}
+        bgRepeat="no-repeat"
+        rounded="2xl"
+        pos="relative"
+      >
+        {children}
+      </Box>
     </Box>
   )
 }
