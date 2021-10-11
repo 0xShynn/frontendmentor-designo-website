cb3eb75377e08fe516465b96ecc5382eda1c9b21
Antonin Nhek <a.nhek@icloud.com>
2021-10-11 15:57:08 +0200

    update: location item (base, sm, md)

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -4,18 +4,22 @@ import NextImage from 'next/image'
 
 const LocationItem = ({ title, address, contact, map }) => {
   return (
-    <Stack direction={{ base: 'column-reverse' }} spacing="0">
+    <Stack
+      direction={{ base: 'column-reverse' }}
+      spacing={{ base: 0, md: 8 }}
+      px={{ md: 8 }}
+    >
       <Flex
         bg="secondary.verylightpeach"
         flex="1"
         direction={{ base: 'column' }}
         py={{ base: 20, md: 20, lg: 24, xl: 24 }}
         px={{ base: 4, md: 16, lg: 16, xl: 20 }}
-        textAlign={{ base: 'center', lg: 'left' }}
+        textAlign={{ base: 'center', md: 'left' }}
         rounded={{ base: 'unset', md: '2xl' }}
         bgPos={{
-          base: '0 bottom',
-          md: 'right 130%',
+          base: 'top bottom',
+          md: '0 bottom',
           lg: '-200% bottom',
           xl: '0 bottom',
         }}
@@ -25,7 +29,7 @@ const LocationItem = ({ title, address, contact, map }) => {
         {title && (
           <Heading
             as="h2"
-            variant="h2"
+            variant="h1"
             letterSpacing="0"
             mb={{ base: 6, lg: 8 }}
             color="primary.peach"
@@ -34,15 +38,18 @@ const LocationItem = ({ title, address, contact, map }) => {
           </Heading>
         )}
         {(address || contact) && (
-          <Stack direction={{ base: 'column', lg: 'row' }} spacing="24">
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            spacing={{ base: 8, md: 28 }}
+          >
             {address && (
-              <Box color="red">
+              <Box color="primary.black">
                 <MDXRemote {...address} />
               </Box>
             )}
 
             {contact && (
-              <Box color="red">
+              <Box color="primary.black">
                 <MDXRemote {...contact} />
               </Box>
             )}
