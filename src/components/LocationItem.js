8962e47d9c0c9cb3054606d11a49e2e08fe7b87e
Antonin Nhek <a.nhek@icloud.com>
2021-10-11 20:20:53 +0200

    test: mapbox secret token

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -80,7 +80,7 @@ const LocationItem = ({ title, address, contact, mapLocation, index }) => {
         >
           <ReactMapGL
             mapStyle="mapbox://styles/mapbox/streets-v9"
-            mapboxApiAccessToken={process.env.MAPBOX_DESIGNO_TOKEN}
+            mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_DESIGNO_TOKEN}
             onViewportChange={(viewport) => setViewport(viewport)}
             {...viewport}
           >
