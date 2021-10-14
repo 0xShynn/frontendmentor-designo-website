bccb168a384d832c2b16ae258d58cd5148363064
Antonin Nhek <a.nhek@icloud.com>
2021-10-14 17:35:22 +0200

    add: react intersection observer, feature comp animation (starter)

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,56 +1,67 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import { InView } from 'react-intersection-observer'
 
 import rotateBackgroundImage from '../utils/rotateBackgroundImage'
 
 import CustomImage from './utils/CustomImage'
 
 const Feature = ({ title, description, image, index }) => {
+  const MotionFlex = motion(Flex)
+
   return (
-    <Flex
-      w="full"
-      justify="center"
-      align="center"
-      direction={{ base: 'column', md: 'row', lg: 'column' }}
-      maxW={{ base: '350px', md: '690px' }}
-      px={{ base: 6, md: 0 }}
-    >
-      {image && (
-        <Box pos="relative" mb={{ base: 12, md: 0, lg: 12 }}>
-          <CustomImage
-            image={image.url}
-            width="202px"
-            height="202px"
-            pos="absolute"
-            top="0"
-            left="0"
-            zIndex="overlay"
-            alt={image.altText}
-          />
-          <CustomImage
-            image="/images/bg-pattern-hero-home.svg"
-            width="202px"
-            height="202px"
-            transform={`rotate(${rotateBackgroundImage(index)})`}
-            alt=""
-          />
-        </Box>
-      )}
-
-      {(title || description) && (
-        <Box
-          textAlign={{ base: 'center', md: 'left', lg: 'center' }}
-          ml={{ md: 12, lg: 0 }}
-          flex="1"
-          color="primary.black"
+    <InView threshold="0.25" triggerOnce>
+      {({ inView, ref }) => (
+        <MotionFlex
+          ref={ref}
+          w="full"
+          justify="center"
+          align="center"
+          direction={{ base: 'column', md: 'row', lg: 'column' }}
+          maxW={{ base: '350px', md: '690px' }}
+          px={{ base: 6, md: 0 }}
+          initial={{ opacity: 0 }}
+          animate={inView ? { opacity: 1 } : { opacity: 0 }}
         >
-          <Heading as="h3" variant="h3" mb={{ base: 8, md: 4 }}>
-            {title}
-          </Heading>
+          {image && (
+            <Box pos="relative" mb={{ base: 12, md: 0, lg: 12 }}>
+              <CustomImage
+                image={image.url}
+                width="202px"
+                height="202px"
+                pos="absolute"
+                top="0"
+                left="0"
+                zIndex="overlay"
+                alt={image.altText}
+              />
+              <CustomImage
+                image="/images/bg-pattern-hero-home.svg"
+                width="202px"
+                height="202px"
+                transform={`rotate(${rotateBackgroundImage(index)})`}
+                alt=""
+              />
+            </Box>
+          )}
 
-          <Text>{description}</Text>
-        </Box>
+          {(title || description) && (
+            <Box
+              textAlign={{ base: 'center', md: 'left', lg: 'center' }}
+              ml={{ md: 12, lg: 0 }}
+              flex="1"
+              color="primary.black"
+            >
+              <Heading as="h3" variant="h3" mb={{ base: 8, md: 4 }}>
+                {title}
+              </Heading>
+
+              <Text>{description}</Text>
+            </Box>
+          )}
+        </MotionFlex>
       )}
-    </Flex>
+    </InView>
   )
 }
 
